e421a87ce5a4d7eda3da6fccda2ae582b85b0c31
Neznamy-Samo <46057894+Neznamy-Samo@users.noreply.github.com>
2024-01-09 01:35:24 +0100

    9.1 1:35

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import './assets/templatemo-tiya-golf-club.css'
-import 'bootstrap-icons/font/fonts/bootstrap-icons.woff'
-import 'bootstrap-icons/font/fonts/bootstrap-icons.woff2'
+import '@fortawesome/fontawesome-free/css/all.css';
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
